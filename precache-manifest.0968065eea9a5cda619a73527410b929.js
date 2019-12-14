08746daa6b656255c69d5327a1568c3c80f13d7c
Depala <jdepala@deloitte.com>
2019-12-14 23:22:05 +0530

    Updates

---
+++ precache-manifest.0968065eea9a5cda619a73527410b929.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd4a10c42cff97f51c856790b09fdf1d",
+    "url": "/stunning-spoon/index.html"
+  },
+  {
+    "revision": "6d1c6797aa89b7de9d25",
+    "url": "/stunning-spoon/static/css/main.7aa0a2cd.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/stunning-spoon/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/stunning-spoon/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6d1c6797aa89b7de9d25",
+    "url": "/stunning-spoon/static/js/main.bf548529.chunk.js"
+  },
+  {
+    "revision": "f10b9f680ec645d5f7cc",
+    "url": "/stunning-spoon/static/js/runtime-main.862bec6c.js"
+  }
+]);
